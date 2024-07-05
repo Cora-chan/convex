f7c1bf798b3838fd56024ebdafd9ab1d392cf673
Yue <kangyue9323@gmail.com>
2024-07-05 17:52:19 +1000

    add email
    add email

--- my-app/src/app/Footer.tsx
+++ my-app/src/app/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <div className="border-b h-8 bg-red-500 text-white text-center">
+        <div className="border-b h-12 bg-red-500 text-white text-center">
              <p>Copyright © 2024 Yue Kang</p>
-             <></>
+             <Link className="underline" href="mailto:kangyue9323@gmail.com">Mail to me</Link>     
         </div>
     )
 }
