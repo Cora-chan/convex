e3b6b91ac87b1bae8b4119c4b26860a3ac7c2354
Yue <kangyue9323@gmail.com>
2024-07-08 23:50:22 +1000

    add loading and disable submit btn

--- my-app/src/app/Navbar.tsx
+++ my-app/src/app/Navbar.tsx
@@ -6,8 +6,8 @@ export function Navbar() {
     return (
         <div className="border-b bg-red-500 text-white text-center">
             <div className="container h-12 mx-auto flex justify-between ">
-            <Link className="pt-3" href="https://github.com/Cora-chan/">My Github</Link>
-            <Link className="pt-3" href="https://main--yueeee-k-portfolio-0001.netlify.app/">My Portfolio</Link>
+            <Link className="pt-3" href="https://github.com/Cora-chan/">Yue's Github</Link>
+            <Link className="pt-3" href="https://main--yueeee-k-portfolio-0001.netlify.app/">Yue's Portfolio</Link>
             </div>
             
         </div>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 import { RefObject, SetStateAction, useEffect, useRef, useState } from "react";
 import { Navbar } from "./Navbar";
+import { Diameter } from "lucide-react";
 
 export default function Home() {
   const [sketchId, setSketchId] = useState("");
@@ -13,9 +14,12 @@ export default function Home() {
   const saveSketchMutation = useMutation(api.sketches.saveSketch);
   const sketchQuery = useQuery(api.sketches.getSketch, {sketchId })
   const canvasRef = useRef<ReactSketchCanvasRef>(null)
+  const [loading, setLoading] = useState(false)
+  const [disabled, setDisabled] = useState(false)
   const handleClear = () => {
     canvasRef.current?.clearCanvas(); 
   }
+
   type DefaultValues = {
     prompt:string;
   };
@@ -23,13 +27,7 @@ export default function Home() {
     const defaultValues:DefaultValues = { prompt: "draw a tree"};
     reset(defaultValues);
   }, [])
- 
-
- 
-  function resetCanvas(canvasRef: RefObject<ReactSketchCanvasRef>): void {
-    throw new Error("Function not implemented.");
-  }
-
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="grid grid-col-2 gap-4">
@@ -37,25 +35,31 @@ export default function Home() {
         className="flex flex-col gap-2" 
         onSubmit={handleSubmit(async(formData)=>{
           if (!canvasRef.current) return;
+          setLoading(true);
+          setDisabled(true);
           const image = await canvasRef.current?.exportImage("jpeg");
           const results = await saveSketchMutation({...formData, image});
           console.log(results)
-          setSketchId(results)        
+          setSketchId(results)   
+         
+              
       })}>
       <p>Prompt</p>
       <input className="text-black h-10 text-xl border-solid border-2 border-[#9c9c9c]" {...register("prompt", {required:true})} />
       {errors.prompt && <span>This field is required</span>}
-      <button type="submit" className="bg-red-500 text-white py-2 px-4 rounded-md">Submit</button>
+      <button type="submit" disabled={disabled} className="submitBtn bg-red-500 text-white py-2 px-4 rounded-md active:bg-red-400 hover:bg-red-400 disabled:bg-red-950 ">Submit</button>
       <button type="reset" onClick={()=>
         handleClear()} 
-        className="bg-[#9c9c9c] text-white py-2 px-4 rounded-md">Clear</button>
-      <p>Scribble it!</p>
+        className="bg-[#9c9c9c] text-white py-2 px-4 rounded-md active:bg-[#4d4c4c] hover:bg-[#4d4c4c]">Clear</button>
+      <p>Scribble it!  {loading?<span className="float-right text-red-500 font-bold" id="loading">Loading...</span>: null}</p>
+     
       <ReactSketchCanvas
       ref={canvasRef}
       style={{width: 256, height: 256, border:"2px solid #9c9c9c"}}
       strokeWidth={4}
       strokeColor="black" />
-      {sketchQuery && <img className="mt-3" height={256} width={256} src={sketchQuery.result}/>}
+     
+      {sketchQuery && <img className="mt-3" onLoad={()=>{setLoading(false);setDisabled(false)}} height={256} width={256} src={sketchQuery.result}/>}
     </form>
     </div>
     </main>
